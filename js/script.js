c901dec495a1c9060c67ea5ed6fab0850ba5fa4c
Gias uddin vuiya <giasuddin9071@gmail.com>
2022-07-27 11:56:42 +0600

    player-vs-player virsion 0.3 success message problem solving completed

--- js/script.js
+++ js/script.js
@@ -10,24 +10,7 @@ const mainScoreElm = document.querySelector('.mainSscoreElm');
 const p1ScoreElm = document.querySelector('#player1ScoreElm');
 const p2ScoreElm = document.querySelector('#player2ScoreElm');
 const errorMessageElm = document.querySelector('.errorMessages');
-const successMessageElm = document.querySelector('.successMessJs');
-
-
-let p1Success = `<div class="successMessage"><h2>Player One is Success</h2></div>`;
-let p2Success = `<div class="successMessage"><h2>Player Two is Success</h2></div>`;
-         
-function succesP1(p1Success)
-{
-	let successMessage1 = p1Success;
-    successMessageElm.insertAdjacentHTML('beforebegin',successMessage1);
-}  				
-function succesP2(p2Success)
-{
-	let successMessage2 = p2Success;
-	successMessageElm.insertAdjacentHTML('beforebegin',successMessage2);
-}
-
-
+const successMessageElm = document.querySelector('.successMessage h2');
 
 // default behavour start here
 function p1_p2BtnDisableByDefault()
@@ -42,17 +25,35 @@ function p1_p2BtnInableByDefault()
 	p1BtnElm.removeAttribute('disabled');
 	p2BtnElm.removeAttribute('disabled');
 }
+// clear function
+function resetAllAfterSuccess()
+{
+	p1Score = 0;
+	p2Score = 0;
 
+	// view layer
+	p1ScoreElm.textContent = p1Score;
+	p2ScoreElm.textContent = p2Score;
+}
 
 // data layer
 let score = 0;
 let p1Score = 0;
 let p2Score = 0;
 let trun = 'player1';
+
+// generate random integer 
+//  1-winscore
+function generateRandomInteger(max)
+{
+	return Math.ceil(Math.random() * max)
+}
+
 // view layer
 mainScoreElm.textContent = score;
 p1ScoreElm.textContent = p1Score;
 p2ScoreElm.textContent = p2Score
+
 
 submitBtnElm.addEventListener('click',(evt) => 
 {
@@ -67,6 +68,8 @@ submitBtnElm.addEventListener('click',(evt) =>
 		p1_p2BtnInableByDefault();
 		errorMessageElm.remove()
 	}
+	// clear all number after submit without main score
+	resetAllAfterSuccess();
 	// reset input box
 	resetInputBox()
 });
@@ -85,12 +88,11 @@ resetBtnElm.addEventListener('click',(evt) =>
 	// remove error message
 	errorMessageElm.remove();
 	// success message remove
-	console.log(successMessageElm.textContent = 'reset success message');
-
-	if(document.querySelector('.successMessage'))
-	{
-		console.log('connected')
-	}
+	   successMessageElm.textContent = '';
+       successMessageElm.style.border = '';
+       successMessageElm.style.background = '';
+    // after clicking reset button then submit button will be inable
+	submitBtnElm.removeAttribute('disabled');
 	
 });
 
@@ -114,7 +116,7 @@ p1BtnElm.addEventListener('click',(evt) =>
 {
 	if(trun === 'player1')
 	{
-		p1Score++;
+		p1Score = generateRandomInteger(score);
 		p1ScoreElm.textContent = p1Score;
 		p1BtnElm.setAttribute('disabled','disabled');
 		p2BtnElm.removeAttribute('disabled');
@@ -127,18 +129,12 @@ function p1SuccessMessage(p1Score)
 	if(p1Score === score)
 	{
 		// success message
-		succesP1(p1Success)
-       
-        // as like as by default
-        score = 0;
-		p1Score = 0;
-		p2Score = 0;
-
-		// view layer
-		mainScoreElm.textContent = score;
-		p1ScoreElm.textContent = p1Score;
-		p2ScoreElm.textContent = p2Score;
-
+	   successMessageElm.textContent = 'Player one is success';
+       successMessageElm.style.border = '1px solid #bdbdbdbf';
+       successMessageElm.style.background = '#efefef';
+        
+        // if p1 or p2 success
+        submitBtnElm.setAttribute('disabled','disabled');
 		p1_p2BtnDisableByDefault();
 	}
 }
@@ -146,7 +142,7 @@ p2BtnElm.addEventListener('click',(evt) =>
 {
 	if(trun === 'player2')
 	{
-		p2Score++;
+		p2Score = generateRandomInteger(score);
 		p2ScoreElm.textContent = p2Score;
 		p2BtnElm.setAttribute('disabled','disabled');
 		p1BtnElm.removeAttribute('disabled');
@@ -159,18 +155,12 @@ function p2SuccessMessage(p2Score)
 {
 	if(p2Score === score)
 	{
-		//success message
-        succesP2(p2Success);
-         // as like as by default
-        score = 0;
-		p1Score = 0;
-		p2Score = 0;
-
-		// view layer
-		mainScoreElm.textContent = score;
-		p1ScoreElm.textContent = p1Score;
-		p2ScoreElm.textContent = p2Score
-
+		// success message
+	   successMessageElm.textContent = 'Player two is success';
+       successMessageElm.style.border = '1px solid #bdbdbdbf';
+       successMessageElm.style.background = '#efefef';
+         // if p1 or p2 success
+        submitBtnElm.setAttribute('disabled','disabled');
 		p1_p2BtnDisableByDefault();
 	}
 }
